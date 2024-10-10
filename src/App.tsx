c8094dc8e95bdc2c844284743e991a8dee7d798a
Fabricio1919 <117752438+Fabricio1919@users.noreply.github.com>
2024-10-10 17:18:12 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-// App.tsx
 import { Box, Card, Flex, Tabs } from "@chakra-ui/react";
+import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./componentes/Ui/NavBar";
 import { QueimadasData } from "./mock/QueimadasData";
 import SideBar from "./componentes/Ui/SideBar";
@@ -10,10 +11,23 @@ import Conscientizacao from "./Pages/Conscientizacao";
 import GraficoBarras from "./componentes/Graficos/GraficoBarras";
 import GraficoLinhas from "./componentes/Graficos/GraficoLinhas";
 import GraficoPizza from "./componentes/Graficos/GraficoPizza";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import InformativoCard from "./componentes/informacoes/InformativoCard";
+import { SplashScreen } from "./componentes/SplashScren/SplashScren";
 
 const App = () => {
+  const [showSplash, setShowSplash] = useState(true); 
+
+  const startTimer = () => {
+    setTimeout(() => {
+      setShowSplash(false); 
+    }, 1000); 
+  };
+
+  if (showSplash) {
+    startTimer();
+    return <SplashScreen />; 
+  }
+
   return (
     <Router>
       <Card id="root">
